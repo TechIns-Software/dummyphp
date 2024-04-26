231cf5b28fb32a4bfe471d241f3d192a0840f7f9
pcmagas <pcmagas@disroot.org>
2024-04-26 11:00:46 +0300

    add nginx vhost

--- deploy/install_app.sh
+++ deploy/install_app.sh
@@ -2,4 +2,9 @@
 
 DIR=/var/www/production
 
+rm -rf  /etc/nginx/sites-enabled/default
+
+mv default /etc/nginx/sites-available/
+ln -s  /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+
 chown -R www-data:www-data /var/www/html/*
