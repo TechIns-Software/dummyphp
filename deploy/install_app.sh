ab917123d08fa32a962c1e8c6e894878b1d6bb36
pcmagas <pcmagas@disroot.org>
2024-04-26 11:09:05 +0300

    Install vhost

--- deploy/install_app.sh
+++ deploy/install_app.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
-DIR=/var/www/production
 
-rm -rf  /etc/nginx/sites-enabled/default
+rm -rf /etc/nginx/sites-enabled/default
 
-mv default /etc/nginx/sites-available/
+mv /var/www/html/default /etc/nginx/sites-available/
 ln -s  /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
 
 chown -R www-data:www-data /var/www/html/*
